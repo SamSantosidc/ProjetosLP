9371f369bbbe0f08669f2dbd5645356da1078765
Samuel Santos <samuel.santos@dcx.ufpb.br>
2025-02-25 13:38:27 -0300

    Update VoadoresMain.java

--- src/interfacePolimorfismo/VoadoresMain.java
+++ src/interfacePolimorfismo/VoadoresMain.java
@@ -1,24 +1,37 @@
 package interfacePolimorfismo;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
 
 public class VoadoresMain {
     public static void main(String[] args) {
-        Voador qqVoador = recebeVoador();
-        qqVoador.voar();//chamada polimórfica
-        qqVoador.planar();//chamada polimórfica
-        qqVoador.pousar();//chamada polimórfica
-    }
+        RepositorioDeVoadores repositorio = new RepositorioDeVoadores();
 
+        for (int i = 0; i < 3; i++) {
+            repositorio.adicionaVoador(recebeVoador());
+        }
+
+        for (Voador voador : repositorio.getVoadores()) {
+            System.out.println("Voador selecionado: " + voador.getClass().getSimpleName());
+            voador.voar();
+            voador.planar();
+            voador.pousar();
+            System.out.println();
+        }
+    }
 
     public static Voador recebeVoador() {
         Random r = new Random();
-        if(r.nextBoolean())
-            return new GalinhaVoadora();
-        else
-            return new Drone("do tipo bom!");
+        int escolha = r.nextInt(9); 
 
+        switch (escolha) {
+            case 0:
+                return new GalinhaVoadora();
+            case 1:
+                return new Drone("do tipo bom!");
+            default:
+                return new Aviao("Comercial");
+        }
     }
 }
